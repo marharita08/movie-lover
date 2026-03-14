29f1ce904c19bbc537f77914c2989c3e88fd41a2
marharita08 <rozgon.margarita08@gmail.com>
2026-03-14 11:30:07 +0200

    fix http exception type

--- src/types/exception.type.ts
+++ src/types/exception.type.ts
@@ -1,12 +1,16 @@
 export interface HttpExceptionBody {
   message?: string;
 }
+
 export class HttpException {
+  public readonly response: Response;
   public readonly body?: HttpExceptionBody;
+
   constructor(
-    public readonly response: Response,
+    response: Response,
     body?: HttpExceptionBody,
   ) {
+    this.response = response;
     if (Array.isArray(body?.message)) {
       this.body = { message: body?.message?.join("\n") };
     } else {
